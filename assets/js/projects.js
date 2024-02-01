90bd75738f4b77090dca1f82d09fb09379347d73
namaya <n3amaya@gmail.com>
2024-01-31 21:04:28 -0600

    cleaned up tags

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,20 +1,66 @@
+const colorPalette = [
+  {
+    // red
+    text: "#E53935",
+    background: "#FFEBEE",
+  },
+  {
+    // pink
+    text: "#C2185B",
+    background: "#FCE4EC",
+  },
+  {
+    // purple
+    text: "#7B1FA2",
+    background: "#F3E5F5",
+  },
+  {
+    // blue
+    text: "#0288D1",
+    background: "#E1F5FE",
+  },
+  {
+    // green
+    text: "#388E3C",
+    background: "#E8F5E9",
+  },
+  // {
+  //   // yellow
+  //   text: "#FBC02D",
+  //   background: "#FFFDE7",
+  // },
+  {
+    // orange
+    text: "#F57C00",
+    background: "#FFF3E0",
+  },
+  {
+    // blue gray
+    text: "#455A64",
+    background: "#ECEFF1",
+  },
+];
+
+const skills = {
+  C: {},
+  "Embedded Programming": {},
+  "Vector Graphics": {},
+  "Computer Architecture": {},
+};
+
+for (const [color, value] of Object.entries(skills)) {
+  const randomColor = Math.floor(Math.random() * colorPalette.length);
+  skills[color].color = colorPalette[randomColor].text;
+  skills[color].backgroundColor = colorPalette[randomColor].background;
+}
+
 const projects = [
   {
     title: "Embedded Pokemon FireRed",
     dateCreated: new Date("2016-04-20"),
     summary:
       "A port of the Gameboy Advance game 'Pok&eacute;mon FireRed' for a Texas Instruments' TM4C microcontroller.",
-    skills: [
-      "C",
-      "Embedded Programming",
-      "Vector Graphics",
-      // "Finite State Machines",
-      // "Analog-to-Digital Converters (ADC)",
-      // "Digital-to-Analog Converters (DAC)",
-      // "TM4C123x Microcontroller",
-      // "Keil &mu;Vision 4 IDE",
-      // "Adafruit ST7735 LCD",
-    ],
+    skills: ["Embedded Programming", "Vector Graphics", "C"],
     thumbnail: "assets/images/thumbnails/pokemon.png",
   },
   {
@@ -37,19 +83,23 @@ projects
     $("#project-list").append(`
       <div class="col">
         <div class="card" style="width: 20rem;">
-          <img src="${project.thumbnail}" class="card-img-top img-thumbnail" alt="..." style="height: 10rem; width: 20rem;">
+          <img src="${project.thumbnail}" class="card-img-top w-100" alt="..." style="height: 10rem;">
           <div class="card-body">
             <h5 class="card-title">${project.title}</h5>
-            <div id="${project.id}-skills" class="d-inline-flex flex-wrap"></div>
             <p class="card-text">${project.summary}</p>
+            <div id="${project.id}-skills" class="d-inline-flex flex-wrap"></div>
           </div>
         </div>
       </div>
     `);
 
     project.skills.forEach((skill) =>
-      $(`#${project.id}-skills`).append(
-        `<small class="text-nowrap px-1">${skill}</small>`
-      )
+      $(`#${project.id}-skills`).append(`
+        <div class="p-1">
+          <small class="text-nowrap px-2 py-1 rounded-pill" style="color: ${skills[skill].color}; background-color: ${skills[skill].backgroundColor};">
+            ${skill}
+          </small>
+        </div>
+        `)
     );
   });
